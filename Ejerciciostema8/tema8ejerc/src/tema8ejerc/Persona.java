f027e0d2337aaf7681ff9a5fdd612d324d8979fc
Sergio <sergioschinca7@gmail.com>
2022-04-14 18:18:50 -0300

    tema 8

---
+++ Ejerciciostema8/tema8ejerc/src/tema8ejerc/Persona.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tema8ejerc;
+
+/**
+ *
+ * @author Sergio
+ */
+public class Persona {
+    private int edad;
+    private String nombre;
+    private String telefono;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Persona{");
+        sb.append("edad=").append(edad);
+        sb.append(", nombre=").append(nombre);
+        sb.append(", telefono=").append(telefono);
+        sb.append('}');
+        return sb.toString();
+    }
+
+
+
+
+    
+    
+}
---
+++ Ejerciciostema8/tema8ejerc/src/tema8ejerc/Tema8ejerc.java
@@ -0,0 +1,18 @@
+package tema8ejerc;
+
+public class Tema8ejerc {
+    public static void main(String[] args) {
+
+        Persona p = new Persona();
+        p.setNombre("Ariel");
+        p.setEdad(18);
+        p.setTelefono("123456789");
+
+        System.out.println(p.toString());
+
+
+
+
+    } // fin main
+
+} //fin class
